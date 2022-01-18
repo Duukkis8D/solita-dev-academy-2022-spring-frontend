b0303d644967492c04b406e7c4955709fc858c96
Tuukka Virtanen <duukkis@gmail.com>
2022-01-18 02:19:57 +0200

    dummy dropdown menu for choosing farm

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
+import React, { useState } from 'react'
+
 const App = () => {
+	const [ farm, setFarm ] = useState( '' )
+
+	const handleFarmChange = ( event ) => {
+		setFarm( event.target.value )
+	}
+
 	return (
-		<div id='appContainer'>Hello World</div>
+		<div id='appContainer'>
+			<h1>Farm data viewer</h1>
+			<label htmlFor='farms'>
+				Please select a farm.
+			</label><br></br>
+			<select name='farms' onChange={ handleFarmChange } value={ farm }>
+				<option defaultValue disabled hidden style={ { display: 'none' } } value=''></option>
+				<option value='partialTechResearchFarm'>PartialTech Research Farm</option>
+				<option value='frimanMetsolaCollective'>Friman Metsola collective</option>
+			</select>
+		</div>
 	)
 }
 
