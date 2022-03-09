fd24b42fdff20a1d22bfc1843d57f2071a647eef
Tuukka Virtanen <duukkis@gmail.com>
2022-03-10 00:23:47 +0200

    add test data for Redux store

---
+++ data/FarmTestData.js
@@ -0,0 +1,704 @@
+export const FarmTestData = () => {
+	[
+		{
+			location: 'PartialTech Research Farm',
+			datetime: '2019-01-01T17:59:36.136Z',
+			sensorType: 'temperature',
+			value: -10.2,
+			_id: '61cdea573e778bd87df61593'
+	  },
+	  {
+			location: 'PartialTech Research Farm',
+			datetime: '2019-01-01T13:53:06.706Z',
+			sensorType: 'temperature',
+			value: -8.9,
+			_id: '61cdea573e778bd87df61598'
+	  },
+	  {
+			location: 'PartialTech Research Farm',
+			datetime: '2019-01-01T11:51:40.150Z',
+			sensorType: 'temperature',
+			value: -8,
+			_id: '61cdea573e778bd87df61595'
+	  },
+	  {
+			location: 'PartialTech Research Farm',
+			datetime: '2019-01-03T21:30:51.321Z',
+			sensorType: 'temperature',
+			value: -15.8,
+			_id: '61cdea573e778bd87df615a7'
+	  },
+	  {
+			location: 'PartialTech Research Farm',
+			datetime: '2019-01-05T16:11:29.370Z',
+			sensorType: 'temperature',
+			value: -15.9,
+			_id: '61cdea573e778bd87df615ba'
+	  },
+	  {
+			location: 'PartialTech Research Farm',
+			datetime: '2019-01-08T18:51:55.908Z',
+			sensorType: 'temperature',
+			value: -7.4,
+			_id: '61cdea573e778bd87df615c7'
+	  },
+	  {
+			location: 'PartialTech Research Farm',
+			datetime: '2019-01-11T17:12:07.625Z',
+			sensorType: 'temperature',
+			value: -15,
+			_id: '61cdea573e778bd87df615e9'
+	  },
+	  {
+			location: 'PartialTech Research Farm',
+			datetime: '2019-01-11T23:55:13.630Z',
+			sensorType: 'temperature',
+			value: -11.6,
+			_id: '61cdea573e778bd87df615f0'
+	  },
+	  {
+			location: 'PartialTech Research Farm',
+			datetime: '2019-01-14T07:51:14.575Z',
+			sensorType: 'rainFall',
+			value: 2,
+			_id: '61cdea573e778bd87df615fe'
+	  },
+	  {
+			location: 'PartialTech Research Farm',
+			datetime: '2019-01-18T05:38:34.404Z',
+			sensorType: 'temperature',
+			value: -16.1,
+			_id: '61cdea573e778bd87df6161a'
+	  },
+	  {
+			location: 'PartialTech Research Farm',
+			datetime: '2019-01-19T10:33:55.721Z',
+			sensorType: 'temperature',
+			value: -14.2,
+			_id: '61cdea573e778bd87df6162b'
+	  },
+	  {
+			location: 'PartialTech Research Farm',
+			datetime: '2019-01-20T10:43:47.717Z',
+			sensorType: 'temperature',
+			value: -15.2,
+			_id: '61cdea573e778bd87df61630'
+	  },
+	  {
+			location: 'PartialTech Research Farm',
+			datetime: '2019-01-24T11:29:41.436Z',
+			sensorType: 'rainFall',
+			value: 0,
+			_id: '61cdea573e778bd87df6164f'
+	  },
+	  {
+			location: 'PartialTech Research Farm',
+			datetime: '2019-01-26T11:23:21.125Z',
+			sensorType: 'rainFall',
+			value: 1.4,
+			_id: '61cdea573e778bd87df61651'
+	  },
+	  {
+			location: 'PartialTech Research Farm',
+			datetime: '2019-01-31T17:11:36.943Z',
+			sensorType: 'rainFall',
+			value: 0,
+			_id: '61cdea573e778bd87df61674'
+	  },
+	  {
+			location: 'PartialTech Research Farm',
+			datetime: '2019-01-31T23:27:29.835Z',
+			sensorType: 'temperature',
+			value: -14.8,
+			_id: '61cdea573e778bd87df61681'
+	  },
+	  {
+			location: 'PartialTech Research Farm',
+			datetime: '2019-02-02T18:19:48.952Z',
+			sensorType: 'pH',
+			value: 5.37,
+			_id: '61cdea573e778bd87df61685'
+	  },
+	  {
+			location: 'PartialTech Research Farm',
+			datetime: '2019-02-03T20:39:31.053Z',
+			sensorType: 'temperature',
+			value: -10.8,
+			_id: '61cdea573e778bd87df61687'
+	  },
+	  {
+			location: 'PartialTech Research Farm',
+			datetime: '2019-02-19T23:40:34.880Z',
+			sensorType: 'temperature',
+			value: -1.3,
+			_id: '61cdea573e778bd87df61702'
+	  },
+	  {
+			location: 'PartialTech Research Farm',
+			datetime: '2019-02-22T05:18:56.509Z',
+			sensorType: 'rainFall',
+			value: 1.8,
+			_id: '61cdea573e778bd87df61714'
+	  },
+	  {
+			location: 'PartialTech Research Farm',
+			datetime: '2019-02-23T01:06:37.941Z',
+			sensorType: 'rainFall',
+			value: 0,
+			_id: '61cdea573e778bd87df6171e'
+	  },
+	  {
+			location: 'PartialTech Research Farm',
+			datetime: '2019-02-25T15:25:09.672Z',
+			sensorType: 'temperature',
+			value: -2,
+			_id: '61cdea573e778bd87df61737'
+	  },
+	  {
+			location: 'PartialTech Research Farm',
+			datetime: '2019-02-25T14:57:33.076Z',
+			sensorType: 'temperature',
+			value: -2.7,
+			_id: '61cdea573e778bd87df6173a'
+	  },
+	  {
+			location: 'PartialTech Research Farm',
+			datetime: '2019-02-26T17:36:12.483Z',
+			sensorType: 'temperature',
+			value: -1.4,
+			_id: '61cdea573e778bd87df6173d'
+	  },
+	  {
+			location: 'PartialTech Research Farm',
+			datetime: '2019-02-26T15:53:13.191Z',
+			sensorType: 'temperature',
+			value: 0.8,
+			_id: '61cdea573e778bd87df61742'
+	  },
+	  {
+			location: 'PartialTech Research Farm',
+			datetime: '2019-02-27T14:29:53.700Z',
+			sensorType: 'temperature',
+			value: 0.8,
+			_id: '61cdea573e778bd87df61753'
+	  },
+	  {
+			location: 'PartialTech Research Farm',
+			datetime: '2019-03-07T02:47:05.727Z',
+			sensorType: 'temperature',
+			value: 8.7,
+			_id: '61cdea573e778bd87df6177e'
+	  },
+	  {
+			location: 'PartialTech Research Farm',
+			datetime: '2019-03-07T11:11:29.184Z',
+			sensorType: 'temperature',
+			value: 9.7,
+			_id: '61cdea573e778bd87df61783'
+	  },
+	  {
+			location: 'PartialTech Research Farm',
+			datetime: '2019-03-07T07:51:25.433Z',
+			sensorType: 'temperature',
+			value: 10.9,
+			_id: '61cdea573e778bd87df61784'
+	  },
+	  {
+			location: 'PartialTech Research Farm',
+			datetime: '2019-01-01T05:38:33.564Z',
+			sensorType: 'temperature',
+			value: -10,
+			_id: '61cdea573e778bd87df6159b'
+	  },
+	  {
+			location: 'PartialTech Research Farm',
+			datetime: '2019-01-01T14:58:20.491Z',
+			sensorType: 'temperature',
+			value: -8.2,
+			_id: '61cdea573e778bd87df6159a'
+	  },
+	  {
+			location: 'PartialTech Research Farm',
+			datetime: '2019-01-04T10:15:52.220Z',
+			sensorType: 'temperature',
+			value: -17.5,
+			_id: '61cdea573e778bd87df615b4'
+	  },
+	  {
+			location: 'PartialTech Research Farm',
+			datetime: '2019-01-04T01:34:36.099Z',
+			sensorType: 'temperature',
+			value: -21.1,
+			_id: '61cdea573e778bd87df615b0'
+	  },
+	  {
+			location: 'PartialTech Research Farm',
+			datetime: '2019-01-05T01:25:29.206Z',
+			sensorType: 'temperature',
+			value: -18.1,
+			_id: '61cdea573e778bd87df615b9'
+	  },
+	  {
+			location: 'PartialTech Research Farm',
+			datetime: '2019-01-06T16:11:29.370Z',
+			sensorType: 'rainFall',
+			value: 0,
+			_id: '61cdea573e778bd87df615bc'
+	  },
+	  {
+			location: 'PartialTech Research Farm',
+			datetime: '2019-01-15T10:26:53.779Z',
+			sensorType: 'temperature',
+			value: -15.3,
+			_id: '61cdea573e778bd87df61607'
+	  },
+	  {
+			location: 'PartialTech Research Farm',
+			datetime: '2019-01-18T14:51:00.184Z',
+			sensorType: 'temperature',
+			value: -15.1,
+			_id: '61cdea573e778bd87df61619'
+	  },
+	  {
+			location: 'PartialTech Research Farm',
+			datetime: '2019-01-19T03:18:08.503Z',
+			sensorType: 'rainFall',
+			value: 0,
+			_id: '61cdea573e778bd87df61626'
+	  },
+	  {
+			location: 'PartialTech Research Farm',
+			datetime: '2019-01-18T23:54:00.694Z',
+			sensorType: 'temperature',
+			value: -19.6,
+			_id: '61cdea573e778bd87df6162d'
+	  },
+	  {
+			location: 'PartialTech Research Farm',
+			datetime: '2019-01-20T03:30:31.361Z',
+			sensorType: 'temperature',
+			value: -15.4,
+			_id: '61cdea573e778bd87df61636'
+	  },
+	  {
+			location: 'PartialTech Research Farm',
+			datetime: '2019-01-22T06:16:28.415Z',
+			sensorType: 'temperature',
+			value: -10.5,
+			_id: '61cdea573e778bd87df61643'
+	  },
+	  {
+			location: 'PartialTech Research Farm',
+			datetime: '2019-01-27T21:08:08.104Z',
+			sensorType: 'temperature',
+			value: -16.6,
+			_id: '61cdea573e778bd87df6165d'
+	  },
+	  {
+			location: 'PartialTech Research Farm',
+			datetime: '2019-01-28T03:43:43.635Z',
+			sensorType: 'temperature',
+			value: -8.4,
+			_id: '61cdea573e778bd87df61661'
+	  },
+	  {
+			location: 'PartialTech Research Farm',
+			datetime: '2019-01-29T10:25:39.814Z',
+			sensorType: 'temperature',
+			value: -9,
+			_id: '61cdea573e778bd87df61668'
+	  },
+	  {
+			location: 'PartialTech Research Farm',
+			datetime: '2019-02-02T16:46:46.291Z',
+			sensorType: 'temperature',
+			value: -4,
+			_id: '61cdea573e778bd87df6168c'
+	  },
+	  {
+			location: 'PartialTech Research Farm',
+			datetime: '2019-02-04T09:57:56.822Z',
+			sensorType: 'temperature',
+			value: 0.5,
+			_id: '61cdea573e778bd87df6168e'
+	  },
+	  {
+			location: 'PartialTech Research Farm',
+			datetime: '2019-02-07T03:03:10.343Z',
+			sensorType: 'pH',
+			value: 5.3,
+			_id: '61cdea573e778bd87df61696'
+	  },
+	  {
+			location: 'PartialTech Research Farm',
+			datetime: '2019-02-08T18:01:33.059Z',
+			sensorType: 'temperature',
+			value: -1.3,
+			_id: '61cdea573e778bd87df616a6'
+	  },
+	  {
+			location: 'PartialTech Research Farm',
+			datetime: '2019-02-11T03:14:49.016Z',
+			sensorType: 'temperature',
+			value: -0.2,
+			_id: '61cdea573e778bd87df616c1'
+	  },
+	  {
+			location: 'PartialTech Research Farm',
+			datetime: '2019-02-12T16:49:19.730Z',
+			sensorType: 'temperature',
+			value: -6.1,
+			_id: '61cdea573e778bd87df616c4'
+	  },
+	  {
+			location: 'Friman Metsola collective',
+			datetime: '2018-12-31T23:10:20.551Z',
+			sensorType: 'temperature',
+			value: -13.9,
+			_id: '61cdec3f172bb01849c2a050'
+	  },
+	  {
+			location: 'Friman Metsola collective',
+			datetime: '2019-01-03T04:01:58.753Z',
+			sensorType: 'temperature',
+			value: -3.9,
+			_id: '61cdec3f172bb01849c2a069'
+	  },
+	  {
+			location: 'Friman Metsola collective',
+			datetime: '2019-01-02T16:55:03.108Z',
+			sensorType: 'temperature',
+			value: -13.3,
+			_id: '61cdec3f172bb01849c2a054'
+	  },
+	  {
+			location: 'Friman Metsola collective',
+			datetime: '2019-01-03T03:52:56.481Z',
+			sensorType: 'temperature',
+			value: -5.2,
+			_id: '61cdec3f172bb01849c2a064'
+	  },
+	  {
+			location: 'Friman Metsola collective',
+			datetime: '2019-01-02T16:09:29.932Z',
+			sensorType: 'temperature',
+			value: -13.4,
+			_id: '61cdec3f172bb01849c2a055'
+	  },
+	  {
+			location: 'Friman Metsola collective',
+			datetime: '2019-01-01T22:11:51.693Z',
+			sensorType: 'temperature',
+			value: -18.1,
+			_id: '61cdec3f172bb01849c2a056'
+	  },
+	  {
+			location: 'Friman Metsola collective',
+			datetime: '2019-01-03T13:38:57.902Z',
+			sensorType: 'temperature',
+			value: -4.1,
+			_id: '61cdec3f172bb01849c2a062'
+	  },
+	  {
+			location: 'Friman Metsola collective',
+			datetime: '2019-01-07T20:28:10.445Z',
+			sensorType: 'temperature',
+			value: -12.6,
+			_id: '61cdec3f172bb01849c2a084'
+	  },
+	  {
+			location: 'Friman Metsola collective',
+			datetime: '2019-01-10T22:06:41.139Z',
+			sensorType: 'temperature',
+			value: -1.1,
+			_id: '61cdec3f172bb01849c2a094'
+	  },
+	  {
+			location: 'Friman Metsola collective',
+			datetime: '2019-01-09T19:20:48.627Z',
+			sensorType: 'temperature',
+			value: 1,
+			_id: '61cdec3f172bb01849c2a098'
+	  },
+	  {
+			location: 'Friman Metsola collective',
+			datetime: '2019-01-14T17:41:33.709Z',
+			sensorType: 'temperature',
+			value: -0.9,
+			_id: '61cdec3f172bb01849c2a0b0'
+	  },
+	  {
+			location: 'Friman Metsola collective',
+			datetime: '2019-01-17T05:40:06.021Z',
+			sensorType: 'temperature',
+			value: -3.5,
+			_id: '61cdec3f172bb01849c2a0c9'
+	  },
+	  {
+			location: 'Friman Metsola collective',
+			datetime: '2019-01-18T02:10:54.968Z',
+			sensorType: 'temperature',
+			value: -9.3,
+			_id: '61cdec3f172bb01849c2a0d6'
+	  },
+	  {
+			location: 'Friman Metsola collective',
+			datetime: '2019-01-20T01:42:34.194Z',
+			sensorType: 'temperature',
+			value: -6.7,
+			_id: '61cdec3f172bb01849c2a0e3'
+	  },
+	  {
+			location: 'Friman Metsola collective',
+			datetime: '2019-01-22T07:05:44.427Z',
+			sensorType: 'temperature',
+			value: -6.8,
+			_id: '61cdec3f172bb01849c2a0f5'
+	  },
+	  {
+			location: 'Friman Metsola collective',
+			datetime: '2019-01-25T11:33:58.129Z',
+			sensorType: 'temperature',
+			value: -3.9,
+			_id: '61cdec3f172bb01849c2a111'
+	  },
+	  {
+			location: 'Friman Metsola collective',
+			datetime: '2019-01-29T15:22:35.083Z',
+			sensorType: 'temperature',
+			value: 5.1,
+			_id: '61cdec3f172bb01849c2a12e'
+	  },
+	  {
+			location: 'Friman Metsola collective',
+			datetime: '2019-01-31T06:42:28.368Z',
+			sensorType: 'temperature',
+			value: -2.2,
+			_id: '61cdec3f172bb01849c2a141'
+	  },
+	  {
+			location: 'Friman Metsola collective',
+			datetime: '2019-02-01T06:42:28.368Z',
+			sensorType: 'pH',
+			value: 5.98,
+			_id: '61cdec3f172bb01849c2a142'
+	  },
+	  {
+			location: 'Friman Metsola collective',
+			datetime: '2019-02-01T06:42:28.368Z',
+			sensorType: 'rainFall',
+			value: 1.7,
+			_id: '61cdec3f172bb01849c2a143'
+	  },
+	  {
+			location: 'Friman Metsola collective',
+			datetime: '2019-02-01T04:47:21.045Z',
+			sensorType: 'temperature',
+			value: -0.8,
+			_id: '61cdec3f172bb01849c2a144'
+	  },
+	  {
+			location: 'Friman Metsola collective',
+			datetime: '2019-02-02T05:53:03.822Z',
+			sensorType: 'temperature',
+			value: -1.9,
+			_id: '61cdec3f172bb01849c2a14d'
+	  },
+	  {
+			location: 'Friman Metsola collective',
+			datetime: '2019-02-02T09:49:39.200Z',
+			sensorType: 'temperature',
+			value: -2,
+			_id: '61cdec3f172bb01849c2a14e'
+	  },
+	  {
+			location: 'Friman Metsola collective',
+			datetime: '2019-02-05T03:56:52.279Z',
+			sensorType: 'temperature',
+			value: 6.3,
+			_id: '61cdec3f172bb01849c2a165'
+	  },
+	  {
+			location: 'Friman Metsola collective',
+			datetime: '2019-02-08T00:35:48.111Z',
+			sensorType: 'temperature',
+			value: 8.3,
+			_id: '61cdec3f172bb01849c2a17f'
+	  },
+	  {
+			location: 'Friman Metsola collective',
+			datetime: '2019-02-09T20:55:02.517Z',
+			sensorType: 'temperature',
+			value: 2.3,
+			_id: '61cdec3f172bb01849c2a189'
+	  },
+	  {
+			location: 'Friman Metsola collective',
+			datetime: '2019-02-11T22:53:36.385Z',
+			sensorType: 'temperature',
+			value: -4.5,
+			_id: '61cdec3f172bb01849c2a1a8'
+	  },
+	  {
+			location: 'Friman Metsola collective',
+			datetime: '2019-02-13T08:35:06.579Z',
+			sensorType: 'pH',
+			value: 6.48,
+			_id: '61cdec3f172bb01849c2a1ad'
+	  },
+	  {
+			location: 'Friman Metsola collective',
+			datetime: '2019-02-12T22:05:43.212Z',
+			sensorType: 'temperature',
+			value: -2.1,
+			_id: '61cdec3f172bb01849c2a1b6'
+	  },
+	  {
+			location: 'Friman Metsola collective',
+			datetime: '2019-02-16T08:25:31.078Z',
+			sensorType: 'temperature',
+			value: 1.9,
+			_id: '61cdec3f172bb01849c2a1d4'
+	  },
+	  {
+			location: 'Friman Metsola collective',
+			datetime: '2019-02-19T01:09:24.191Z',
+			sensorType: 'temperature',
+			value: -3.1,
+			_id: '61cdec3f172bb01849c2a1ef'
+	  },
+	  {
+			location: 'Friman Metsola collective',
+			datetime: '2019-02-21T00:57:08.839Z',
+			sensorType: 'temperature',
+			value: 4.3,
+			_id: '61cdec3f172bb01849c2a1ff'
+	  },
+	  {
+			location: 'Friman Metsola collective',
+			datetime: '2019-02-24T17:03:33.657Z',
+			sensorType: 'temperature',
+			value: 11.3,
+			_id: '61cdec3f172bb01849c2a215'
+	  },
+	  {
+			location: 'Friman Metsola collective',
+			datetime: '2019-02-26T06:01:26.171Z',
+			sensorType: 'temperature',
+			value: 2.8,
+			_id: '61cdec3f172bb01849c2a222'
+	  },
+	  {
+			location: 'Friman Metsola collective',
+			datetime: '2019-02-27T07:46:20.053Z',
+			sensorType: 'temperature',
+			value: 12.6,
+			_id: '61cdec3f172bb01849c2a22f'
+	  },
+	  {
+			location: 'Friman Metsola collective',
+			datetime: '2019-01-04T23:41:58.250Z',
+			sensorType: 'temperature',
+			value: -15.7,
+			_id: '61cdec3f172bb01849c2a07a'
+	  },
+	  {
+			location: 'Friman Metsola collective',
+			datetime: '2019-01-05T17:02:29.724Z',
+			sensorType: 'temperature',
+			value: -14,
+			_id: '61cdec3f172bb01849c2a07e'
+	  },
+	  {
+			location: 'Friman Metsola collective',
+			datetime: '2019-01-16T08:58:38.205Z',
+			sensorType: 'rainFall',
+			value: 2.1,
+			_id: '61cdec3f172bb01849c2a0b9'
+	  },
+	  {
+			location: 'Friman Metsola collective',
+			datetime: '2019-01-18T15:13:04.634Z',
+			sensorType: 'temperature',
+			value: -8,
+			_id: '61cdec3f172bb01849c2a0ce'
+	  },
+	  {
+			location: 'Friman Metsola collective',
+			datetime: '2019-01-18T13:47:17.330Z',
+			sensorType: 'temperature',
+			value: -7.8,
+			_id: '61cdec3f172bb01849c2a0e0'
+	  },
+	  {
+			location: 'Friman Metsola collective',
+			datetime: '2019-01-24T10:24:28.158Z',
+			sensorType: 'temperature',
+			value: 4.3,
+			_id: '61cdec3f172bb01849c2a103'
+	  },
+	  {
+			location: 'Friman Metsola collective',
+			datetime: '2019-01-25T23:15:15.616Z',
+			sensorType: 'temperature',
+			value: 0.1,
+			_id: '61cdec3f172bb01849c2a109'
+	  },
+	  {
+			location: 'Friman Metsola collective',
+			datetime: '2019-01-30T07:29:50.051Z',
+			sensorType: 'temperature',
+			value: 2.1,
+			_id: '61cdec3f172bb01849c2a138'
+	  },
+	  {
+			location: 'Friman Metsola collective',
+			datetime: '2019-02-02T17:15:52.824Z',
+			sensorType: 'rainFall',
+			value: 2.9,
+			_id: '61cdec3f172bb01849c2a14a'
+	  },
+	  {
+			location: 'Friman Metsola collective',
+			datetime: '2019-02-03T11:25:49.645Z',
+			sensorType: 'rainFall',
+			value: 0,
+			_id: '61cdec3f172bb01849c2a155'
+	  },
+	  {
+			location: 'Friman Metsola collective',
+			datetime: '2019-02-04T23:21:45.922Z',
+			sensorType: 'temperature',
+			value: 3.7,
+			_id: '61cdec3f172bb01849c2a168'
+	  },
+	  {
+			location: 'Friman Metsola collective',
+			datetime: '2019-02-07T04:32:16.493Z',
+			sensorType: 'temperature',
+			value: 10.3,
+			_id: '61cdec3f172bb01849c2a170'
+	  },
+	  {
+			location: 'Friman Metsola collective',
+			datetime: '2019-02-07T08:11:12.382Z',
+			sensorType: 'temperature',
+			value: 12.4,
+			_id: '61cdec3f172bb01849c2a178'
+	  },
+	  {
+			location: 'Friman Metsola collective',
+			datetime: '2019-02-08T22:04:50.093Z',
+			sensorType: 'temperature',
+			value: -0.7,
+			_id: '61cdec3f172bb01849c2a186'
+	  },
+	  {
+			location: 'Friman Metsola collective',
+			datetime: '2019-02-09T19:00:45.149Z',
+			sensorType: 'temperature',
+			value: 1.3,
+			_id: '61cdec3f172bb01849c2a18d'
+	  }
+	]
+}
